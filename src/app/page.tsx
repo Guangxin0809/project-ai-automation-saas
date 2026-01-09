22921c6da3eef6e9a7963485f41d4e4999ead55a
Yu19980809 <1572250276@qq.com>
2026-01-09 21:15:41 +0800

    feat: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,43 @@
-import { caller } from "@/trpc/server";
-import { requireAuth } from "@/utils/auth";
+"use client";
+
+import { toast } from "sonner";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
 
 import { LogoutButton } from "./logout-button";
 
-const HomePage = async () => {
 
-  await requireAuth();
+const HomePage = () => {
 
-  const data = await caller.getUsers();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const crateWorkflowMutation = useMutation(trpc.crateWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(
+        trpc.getWorkflows.queryOptions(),
+      );
+    },
+  }));
 
   return (
     <div className="flex flex-col justify-center items-center gap-y-6 min-w-screen min-h-screen">
       protected server component
       <p>{JSON.stringify(data, null, 2)}</p>
+
+      <Button
+        disabled={crateWorkflowMutation.isPending}
+        onClick={() => crateWorkflowMutation.mutate()}
+      >
+        Create workflow
+      </Button>
+
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,27 @@
 import prisma from "@/lib/prisma";
 
+import { inngest } from "@/inngest/client";
+
 import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(() => {
-    return prisma.user.findMany();
+  getWorkflows: protectedProcedure.query(() => {
+    return prisma.workflow.findMany();
+  }),
+  crateWorkflow: protectedProcedure.mutation(async (ctx) => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "guangxinyu1998@gmail.com",
+      },
+    });
+
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow",
+        userId: ctx.ctx.auth.user.id
+      },
+    });
   }),
 });
 
