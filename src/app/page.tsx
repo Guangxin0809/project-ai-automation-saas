d61a96f4dd916cdbf59784c03b1663cd84372226
Yu19980809 <1572250276@qq.com>
2026-01-09 15:16:30 +0800

    feat: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default HomePage;
